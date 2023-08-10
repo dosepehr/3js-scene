b6e0bdbafe437b789dbdaac7b9ea06850b593a37
dosepehr <dosepehr@gmail.com>
2023-08-10 13:53:44 +0330

    add cube and controller

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import * as dat from 'lil-gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /**
  * debug
@@ -7,7 +8,7 @@ import * as dat from 'lil-gui';
 const gui = new dat.GUI();
 
 // canvas
-const canvas = document.querySelector('canvas.webgl');
+const canvas = document.querySelector('canvas.webgl')
 
 /**
  * scene
@@ -23,6 +24,20 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(45, sizes.w / sizes.h, 0.1, 100);
 
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 4;
+scene.add(camera);
+
+/**
+ * cube
+ */
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'white' })
+);
+
+scene.add(cube);
 /**
  * renderer
  */
@@ -33,7 +48,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.w, sizes.h);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+/**
+ * controls
+ */
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true;
+
 const tick = () => {
+    controls.update();
     renderer.render(scene, camera);
 
     window.requestAnimationFrame(tick);
